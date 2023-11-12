e08f0fbc0ae1ef703b002dafaa05f3b346d31b07
Radek <radek.walaszczyk75@gmail.com>
2023-11-12 12:29:15 +0100

    on cos tam ten

--- Scenes/BasicTemporarySpell.gd
+++ Scenes/BasicTemporarySpell.gd
@@ -1,8 +1,23 @@
 extends Node3D
 
-func _ready():
-	pass # Replace with function body.
+var target : Node3D
 
-func _process(delta):
-	global_position += transform.basis * Vector3.FORWARD * 2
+func _physics_process(delta):
+	if target == null:
+		var allPossTargets = $NearestEnemy.get_overlapping_areas()
+		var shortestDistance : float = 150
+		for possTarget in allPossTargets:
+			var currDist = (possTarget.global_position - global_position).length()
+			if currDist < shortestDistance:
+				shortestDistance = currDist
+				target = possTarget
+		return
+
+	look_at(target.global_position)
+	global_position += transform.basis * Vector3.FORWARD
+	pass
+
+
+func _on_hit_collider_area_entered(area: Area3D):
+	queue_free()
 	pass
