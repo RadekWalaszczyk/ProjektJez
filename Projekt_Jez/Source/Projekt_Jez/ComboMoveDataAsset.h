c3a1b31a9b074b676ad15574671dbcb55d55453e
Radek <radek.walaszczyk75@gmail.com>
2024-01-06 19:52:12 +0100

    Database do combosów

---
+++ Projekt_Jez/Source/Projekt_Jez/ComboMoveDataAsset.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "ComboMoveDataAsset.generated.h"
+
+/** Enum of every possible move in combo system */
+UENUM(BlueprintType)
+enum class ESpellMove : uint8
+{
+    VE_Up       UMETA(DisplayName = "Up"),
+    VE_Right    UMETA(DisplayName = "Right"),
+    VE_Down     UMETA(DisplayName = "Down"),
+    VE_Left     UMETA(DisplayName = "Left"),
+};
+
+/**
+ * 
+ */
+UCLASS(BlueprintType)
+class PROJEKT_JEZ_API UComboMoveDataAsset : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+    UPROPERTY(BlueprintReadWrite, EditAnywhere)
+    ESpellMove SpellMove;
+};
